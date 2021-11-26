4289dbfa3364f88be381f0532ff3ea6a576af28c
Rivaldo2117051054 <rivaldorr547@gmail.com>
2021-11-26 09:00:24 +0700

    fungsi dalam c++

---
+++ test_fungsi.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void test(){
+	string all=" Namaku Rivaldo jenis kelamin laki laki alamat lampung selatan";
+	cout<<all<<endl;
+	//fungsi test
+
+}
+int main(){
+	
+	test();
+	//untuk memanggil fungsi test
+}
